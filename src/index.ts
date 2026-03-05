ca311ffe46093bcb02f5db53352b73d27fcf5cd5
Saurav <maharjansaurav2002@gmail.com>
2026-03-05 20:44:17 +0545

    Tests pass

--- src/index.ts
+++ src/index.ts
@@ -26,3 +26,5 @@ initChatSocket(io);
 // ✅ Start server
 server.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running at http://192.168.0.102:${PORT}`);
+  console.log(`Network: ${PORT} (Use this for Mobile/Flutter)`);
+});
